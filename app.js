951ca7eece987ea6fd29f4d40d546a71555e04c4
Non T <117605943+Ratanon691@users.noreply.github.com>
2024-06-22 20:23:58 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-// write code here
+let user = prompt(`Who are you?`)
+if(user == `` || user == null || user !== `codecamp`){
+let user = `Guest`
+  alert(`You logged in as a ${user}`)
+}
+else if(user == `codecamp`){
+  let password = prompt(`Password?`)
+  if(password == 123456){
+    alert(`Hello, ${user}`)
+  }
+  else{
+    alert(`Wrong password`)
+  }
+}
